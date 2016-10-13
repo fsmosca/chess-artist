632d2edbbfeb444d7666cd5972fb399f9095fd82
fsmosca <fsmosca@users.noreply.github.com>
2016-10-13 15:09:55 +0800

    Update run_chess-artist.bat

--- Tools/run_chess-artist.bat
+++ Tools/run_chess-artist.bat
@@ -1,5 +1,12 @@
 :: (1) Analyze pgn file
 
-chess-artist -infile anderssen_dufresne_1852.pgn -outfile out_anderssen_dufresne_1852.pgn -eng Bf.exe -eval search -movetime 5000 -movestart 8
+:: chess-artist -infile anderssen_dufresne_1852.pgn -outfile out_anderssen_dufresne_1852.pgn -eng Bf.exe -eval search -movetime 5000 -movestart 8
+
+
+:: (2) Test engine with epd suite
+
+chess-artist -infile wacnew.epd -outfile out_wacnew.txt -eng Bf.exe -eval search -enghash 128 -engthreads 1 -movetime 300 -job test
+
+
 
 pause
