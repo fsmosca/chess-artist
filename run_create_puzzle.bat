0267756110affc68ffd02a3d0f5af507ea31caaa
fsmosca <fsmosca@users.noreply.github.com>
2020-02-12 22:28:26 +0800

    Add example batch files

---
+++ run_create_puzzle.bat
@@ -0,0 +1,9 @@
+:: Generate puzzles
+
+
+:: Example command line
+
+chess-artist.py --infile pgn\tatagpa20.pgn --outfile out_tatagpa20.pgn --enginename "Lc0 v0.23.2 w591097 blas" --enginefile D:\Chess\Engines\Lc0\lc0-v0.23.2-591097-10X128\lc0.exe --engineoptions "Threads value 2, MinibatchSize value 8, MaxPrefetch value 0" --movestart 15 --movetime 10000 --job createpuzzle --eval search --log
+
+pause
+
