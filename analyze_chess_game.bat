6928eb98c5a70f5a9c0fa7142142f97ff9d840c8
fsmosca <fsmosca@users.noreply.github.com>
2020-11-04 02:54:43 +0800

    Create analyze_chess_game.bat

---
+++ analyze_chess_game.bat
@@ -0,0 +1,6 @@
+:: Analyze games in tatasteel 2020 using Stockfish 12
+
+chess_artist.py --infile ./PGN/tatagpa20.pgn --outfile ana_tatagpa20.pgn --enginefile ./Engine/stockfish/stockfish_12_x64_modern.exe --engineoptions "Threads value 1, Hash value 128" --movestart 8 --movetime 200 --job analyze --eval search --log
+
+
+pause
